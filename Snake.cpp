3570437a0b2cb98dc82f9ebacec25600f9882993
Arthur Galvão dos Santos <86384666+Arthurgalvao645@users.noreply.github.com>
2023-05-25 20:21:38 -0300

    Add files via upload

---
+++ Snake.cpp
@@ -0,0 +1,237 @@
+#include <stdio.h>
+#include <time.h> 
+#include <stdlib.h> 
+#include <strings.h> 
+#include <windows.h> 
+#include <conio.h> 
+#define V 21
+#define H 75
+#define N 100
+
+#define V 21
+#define H 75
+#define N 100
+
+
+
+typedef struct{
+	int x, y; // posição inicial
+	int movX, movY;
+	char imagen;
+
+} snake;
+
+typedef struct{
+
+	int x, y;
+
+} fruit;
+
+snake snk[N]; // tamanho máximo
+fruit frt;
+
+void begin(int *size, char map[V][H]);
+void intro(char map[V][H]);
+void intro_data(char map[V][H], int *size);
+void loop(char map[V][H], int size);
+void input(char map[V][H], int *size, int *dead);
+void update(char map[V][H], int size);
+void intro_data2(char map[V][H], int size);
+void gotoxy(int x, int y); // Esta função foi sugerida por um amigo para obter melhor desempenho
+void show(char map[V][H]);
+
+int main(){
+	int size;
+	char map[V][H];
+	begin(&size, map);
+	show(map);
+	system("pause");
+	loop(map, size);
+	system("pause");
+	return 0;
+}
+
+// Inicializado em uma posição inicial
+void begin(int *size, char map[V][H]){
+	int i;
+	// cabeça de cobra
+	snk[0].x = 32;
+	snk[0].y = 10; // V
+	// corpo de cobra
+	*size = 4;
+	srand(time(NULL));
+	frt.x = rand() % (H - 2) + 1; // posição automática de frutas
+	frt.y = rand() % (V - 2) + 1;
+	for (i = 0; i < *size; i++){
+
+		snk[i].movX = 1;
+		snk[i].movY = 0;
+
+	} // cobra só se move em X
+
+	intro(map);
+	intro_data(map, size);
+}
+
+// Mostrar tudo
+void show(char map[V][H]){
+	int i, j;
+	for (i = 0; i < V; i++){
+
+		for (j = 0; j < H; j++){
+
+			printf("%c", map[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+// O campo (mapa).
+void intro(char map[V][H]){
+	int i, j;
+	for (i = 0; i < V; i++){
+		for (j = 0; j < H; j++){
+			if (i == 0 || i == V - 1){
+
+				map[i][j] = '-';
+			}
+			else if (j == 0 || j == H - 1){
+
+				map[i][j] = '|';
+			}
+			else{
+				map[i][j] = ' ';
+			}
+		}
+	}
+}
+
+// Apresente cada data inIT
+void intro_data(char map[V][H], int *size){
+	int i;
+	for (i = 1; i < *size; i++){
+		snk[i].x = snk[i - 1].x - 1;
+		snk[i].y = snk[i - 1].y;
+
+		snk[i].imagen = 'X'; // Corpo
+	}
+	snk[0].imagen = 'O'; // Cabeça
+
+	// Introduza a cobra em nosso acampamento
+	for (i = 0; i < *size; i++){
+
+		map[snk[i].y][snk[i].x] = snk[i].imagen;
+	}
+
+	map[frt.y][frt.x] = 'M'; // Fruta
+}
+
+void loop(char map[V][H], int size){
+
+	int dead;
+	dead = 0;
+
+	do{
+		gotoxy(0, 0);
+
+		show(map);
+		input(map, &size, &dead);
+		update(map, size); // Automático
+
+	} while (dead == 0);
+}
+
+void input(char map[V][H], int *size, int *dead){
+	int i;
+	char key;
+
+	// Apenas duas maneiras de morrer, colisão com mapa ou corpo, cada parte da cobra é uma estrutura diferente
+
+	// VERIFIQUE AS CONDIÇÕES DO JOGO
+
+	if (snk[0].x == 0 || snk[0].x == H - 1 || snk[0].y == 0 || snk[0].y == V - 1){ // 0 É a cabeça da cobra, só vamos avaliar quando a cabeça colidir.
+
+		*dead = 1;
+	}
+
+	for (i = 1; i < *size && *dead == 0; i++){
+
+		if (snk[0].x == snk[i].x && snk[0].y == snk[i].y)
+		{
+			*dead = 1;
+		}
+	}
+
+	// VERIFIQUE A FRUTA, SE A CABEÇA SE APROXIMAR, COMA!
+
+	if (snk[0].x == frt.x && snk[0].y == frt.y){
+
+		*size += 1;
+
+		snk[*size - 1].imagen = 'X';
+
+		// Posição da fruta regenerada em uma posição aleatória
+
+		frt.x = rand() % (H - 2) + 1;
+		frt.y = rand() % (V - 2) + 1;
+	}
+
+	// SE MORRER É ZERO PODEMOS CONTINUAR
+	if (*dead == 0){
+		if (kbhit() == 1){
+			key = getch();
+
+			if (key == 's' && snk[0].movY != -1){
+				snk[0].movX = 0;
+				snk[0].movY = 1;
+			}
+
+			if (key == 'w' && snk[0].movY != 1){
+				snk[0].movX = 0;
+				snk[0].movY = -1;
+			}
+
+			if (key == 'a' && snk[0].movX != 1){
+				snk[0].movX = -1;
+				snk[0].movY = 0;
+			}
+
+			if (key == 'd' && snk[0].movX != -1){
+				snk[0].movX = 1;
+				snk[0].movY = 0;
+			}
+		}
+	}
+}
+
+void update(char map[V][H], int size){
+	// Toda vez que esta função é chamada, o mapa está sobrescrevendo
+	intro(map);
+	intro_data2(map, size);
+}
+
+void intro_data2(char map[V][H], int size){
+	int i;
+	// Corpo seguindo a cabeça
+	for (i = size - 1; i > 0; i--){ // 0 é a cabeça. então vamos diminuindo até as extremidades
+		snk[i].x = snk[i - 1].x;
+		snk[i].y = snk[i - 1].y;
+	}
+	snk[0].x += snk[0].movX; // mais movimentos
+	snk[0].y += snk[0].movY;
+
+	// agora introduza os valores no acampamento
+	for (i = 0; i < size; i++)
+	{
+
+		map[snk[i].y][snk[i].x] = snk[i].imagen;
+	}
+	map[frt.y][frt.x] = 'M';
+}
+
+void gotoxy(int x, int y){ // eu não sei que diabos é essa função, originalmente eu usei outra, mas isso funciona bem (sorte) :)
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
